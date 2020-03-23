4ece9c9040801e1ff83259713f7c25612b1da5ed
Jon Pepler <jonathan.pepler@gmail.com>
2020-03-23 00:54:43 +0000

    feat: 🎸 Improve asteroid shape generation

--- src/components/asteroids/objects/asteroid.js
+++ src/components/asteroids/objects/asteroid.js
@@ -4,13 +4,26 @@ const size = 100
 class Asteroid extends AstroObject {
   constructor (x, y) {
     super(x, y, 0)
-      .withSize(size)
+      .withSize(Math.random() * (3 / 4 * size) + size)
       .withShape(Asteroid.randomShape())
       .withDelta(Asteroid.randomDelta())
   }
 
   static randomShape () {
-    return [[0, -0.5], [0.5, 0], [0, 0.5], [-0.5, 0]]
+    const numPoints = Math.floor(Math.random() * 15 + 5)
+    const circle = 2 * Math.PI
+    const step = circle / numPoints
+
+    const points = []
+    for (let i = 0; i < circle; i += step) {
+      const length = Math.random() * (2 / 3) + (1 / 3)
+      points.push([
+        length * Math.cos(i),
+        length * Math.sin(i)
+      ])
+    }
+
+    return points
   }
 
   static randomDelta () {
