51001c8468da26554b09124a03a609032e537cc6
Jon Pepler <jonathan.pepler@gmail.com>
2020-03-23 10:39:37 +0000

    fix: 🐛 Fetch from db properly on first load

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,5 +1,5 @@
 import { set as idbSet, get as idbGet } from 'idb-keyval'
 import { getDefault } from './defaults'
 
-export const get = async key => idbGet(key) || getDefault(key) || ''
+export const get = async key => await idbGet(key) || getDefault(key) || ''
 export const set = async (key, value) => idbSet(key, value)
