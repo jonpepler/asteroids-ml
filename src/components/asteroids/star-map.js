73c29f912531c80e1a3be17822e52c2aadad90dc
Jon Pepler <jonathan.pepler@gmail.com>
2020-03-24 14:03:47 +0000

    feat: 🎸 Add a chance for stars to twinkle ✨

--- src/components/asteroids/star-map.js
+++ src/components/asteroids/star-map.js
@@ -5,7 +5,7 @@ class StarMap {
   }
 
   static generate (maxX, maxY) {
-    const starNum = Math.floor(Math.random() * maxX * maxY / 1400)
+    const starNum = Math.floor(Math.random() * maxX * maxY / 2800)
     const random = bound => Math.random() * (bound + 100) - 100
     return new StarMap(
       Array.from({ length: starNum })
@@ -16,8 +16,13 @@ class StarMap {
   draw (p5) {
     p5.push()
     p5.stroke(255)
-    p5.strokeWeight(1)
-    this.stars.forEach(star => p5.point(...star))
+    p5.strokeWeight(2)
+    this.stars.forEach(star => {
+      p5.push()
+      if (Math.random() < 0.0005) p5.strokeWeight(4)
+      p5.point(...star)
+      p5.pop()
+    })
     p5.pop()
   }
 
